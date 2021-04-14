4dde495ebc7691044d2d2e7e3c4ff9ed78743286
Shah Raza <63047096+ShahSomething@users.noreply.github.com>
2021-04-14 22:06:51 +0500

    Create main.cpp

---
+++ 2nd Semester/Computer Programming/Lab/Tasks/Lab 1/Product/Product/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A,B,Product;
+    A=10;B=8;
+    Product=A*B;
+    cout<<"The Product of two numbers is ";
+    cout<<Product;
+    return 0;
+}
