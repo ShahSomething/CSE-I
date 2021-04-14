9f5dfb88409d0bdaad624228eaa6e76a1fab6e94
Shah Raza <63047096+ShahSomething@users.noreply.github.com>
2021-04-14 21:59:33 +0500

    Add files via upload

---
+++ 2nd Semester/Computer Programming/Lab/Tasks/Lab 1/Area of Circle/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int r;
+    r=5;
+    float area=3.14*r*r;
+    cout<<"Area of circle of radius "<<r<<" is "<<area;
+    return 0;
+}
