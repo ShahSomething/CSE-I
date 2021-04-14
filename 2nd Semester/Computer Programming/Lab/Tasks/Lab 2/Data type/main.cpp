b9c849db92885092f391c159c7af523b493aae88
Shah Raza <63047096+ShahSomething@users.noreply.github.com>
2021-04-14 22:14:41 +0500

    Create main.cpp

---
+++ 2nd Semester/Computer Programming/Lab/Tasks/Lab 2/Data type/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*Variable declaration
+    and assignment*/
+    //integers
+    int A=10,B=15;
+    //floating point
+    float C=12.6;
+    //Display integers
+    cout<<"Integer variables: "<<endl;
+    cout<<"A= "<< A <<endl;
+    cout<<"B= "<< B <<endl<<endl;
+    //Display floating point
+    cout<<"Float variable: "<<endl;
+    cout<<"C= "<< C <<endl;
+
+    return 0;
+}
