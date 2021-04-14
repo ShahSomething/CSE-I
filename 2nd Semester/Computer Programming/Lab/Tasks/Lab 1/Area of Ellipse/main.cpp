2adb3b8eed4fb6f168e82c5d104b7b60142799f5
Shah Raza <63047096+ShahSomething@users.noreply.github.com>
2021-04-14 22:01:41 +0500

    Create main.cpp

---
+++ 2nd Semester/Computer Programming/Lab/Tasks/Lab 1/Area of Ellipse/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int minor,major;
+    minor=4;major=6;
+    float area=3.14*minor*major;
+    cout<<"Area of Ellipse is "<<area;
+    return 0;
+}
