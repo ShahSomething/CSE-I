736fe5f2776680065e19d96c45966cc07adfa264
Shah Raza <63047096+ShahSomething@users.noreply.github.com>
2021-04-14 22:09:21 +0500

    Create main.cpp

---
+++ 2nd Semester/Computer Programming/Lab/Tasks/Lab 2/Size/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Data types with their sizes
+    cout << "Sizes of Data Types " << endl<<endl;
+    //char
+    cout << "Size of char: " << sizeof(char) <<" Byte(s)"<< endl;
+    //float
+    cout << "Size of float: " << sizeof(float) <<" Byte(s)"<< endl;
+    //double
+    cout << "Size of double: " << sizeof(double) <<" Byte(s)"<< endl;
+    //long double
+    cout << "Size of long double : " << sizeof(long double) <<" Byte(s)"<< endl;
+    return 0;
+}
