52d4ecf7b45cc21cd7cba8bdcc747af3cd1867e2
Shah Raza <63047096+ShahSomething@users.noreply.github.com>
2021-04-14 21:57:44 +0500

    Add files via upload

---
+++ 2nd Semester/Computer Programming/Lab/Tasks/Lab 1/Addition of two numbers/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A,B,sum;
+    A=5;B=7;
+    sum=A+B;
+    cout<<"The sum of two numbers is ";
+    cout<<sum;
+    return 0;
+}
