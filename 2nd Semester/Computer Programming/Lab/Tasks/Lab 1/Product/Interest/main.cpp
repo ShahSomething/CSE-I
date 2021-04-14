27451b3da42866e8f4945b1080148dfb099df799
Shah Raza <63047096+ShahSomething@users.noreply.github.com>
2021-04-14 22:03:26 +0500

    Create main.cpp

---
+++ 2nd Semester/Computer Programming/Lab/Tasks/Lab 1/Product/Interest/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int P,T;
+    float R,I;
+    P=4000;T=2;R=5.5;
+    I=(P*T*R)/100;
+    cout << "The Simple interest is: ";
+    cout << I;
+
+    return 0;
+}
