932423b2d865b04d4a58c857c9ea9d8d79e8a4dd
Shah Raza <63047096+ShahSomething@users.noreply.github.com>
2021-04-16 04:11:21 +0500

    Create main.cpp

---
+++ 3rd Semester/OOP Lab/Lab 06/Task 3/Task 3.1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "First.h"
+#include "Second.h"
+#include "Third.h"
+using namespace std;
+
+int main()
+{
+    Third t1;
+    t1.t_input();
+    t1.Show();
+    t1.Max();
+    return 0;
+}
