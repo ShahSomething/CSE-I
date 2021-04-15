2dba7ecd85167b0a174746188bdeb5a55a706e5d
Shah Raza <63047096+ShahSomething@users.noreply.github.com>
2021-04-15 18:58:09 +0500

    Create main.cpp

---
+++ 2nd Semester/Computer Programming/Theory/Assignment 2/Output/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"\n\n";
+    cout << "\tx\t|\ty\t|\texpressions \t|\tresults " << endl;
+    cout << "\t6\t|\t3\t|\tx=y+3 	     \t|\tx=6 " << endl;
+    cout << "\t6\t|\t3\t|\tx=y-2 	     \t|\tx=1 " << endl;
+    cout << "\t6\t|\t3\t|\tx=y*5       \t|\tx=15" << endl;
+    cout << "\t6\t|\t3\t|\tx=x/y 	 \t|\tx=2" << endl;
+    cout << "\t6\t|\t3\t|\tx=x%y 	     \t|\tx=0" << endl;
+    return 0;
+}
