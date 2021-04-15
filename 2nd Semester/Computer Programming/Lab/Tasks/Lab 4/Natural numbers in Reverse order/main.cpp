ef99df2eac8ffe7639caa457b34446d80171494f
Shah Raza <63047096+ShahSomething@users.noreply.github.com>
2021-04-15 17:21:04 +0500

    Create main.cpp

---
+++ 2nd Semester/Computer Programming/Lab/Tasks/Lab 4/Natural numbers in Reverse order/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n; //Variable Declaration
+    cout << "Enter the value of n: "; //Display message
+    cin>>n; //Input n
+    for(int i=1;i<=n;n--) //Loop repetition from n to i
+    {
+        cout<<n<<endl; //Display Number
+    }
+    return 0;
+}
