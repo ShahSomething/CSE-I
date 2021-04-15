73c237d8637979f769c6c80ca1ce2952d6063668
Shah Raza <63047096+ShahSomething@users.noreply.github.com>
2021-04-15 18:58:44 +0500

    Create main.cpp

---
+++ 2nd Semester/Computer Programming/Theory/Assignment 2/Output 2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"x\ty\tz=\t3.0|\t\t12.5|\t\t523.3|"<<endl;
+    cout<<"A\tB\tC=\t300.0|\t\t1200.5|\t\t5300.3|\n"<<endl;
+    cout<<"--------------------------------------------------------------------\n";
+    cout<<"x\ty\tz=\t|3.00\t\t|12.50\t\t|523.30"<<endl;
+    cout<<"A\tB\tC=\t|300.00\t\t|1200.50\t|52300.30\n"<<endl;
+    return 0;
+}
