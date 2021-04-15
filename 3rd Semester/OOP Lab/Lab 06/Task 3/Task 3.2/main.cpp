4c481927215bdd72bc5042f7e1e9a2c4e3ab3a7e
Shah Raza <63047096+ShahSomething@users.noreply.github.com>
2021-04-16 04:14:45 +0500

    Create main.cpp

---
+++ 3rd Semester/OOP Lab/Lab 06/Task 3/Task 3.2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <Base.h>
+#include <Exponent.h>
+#include <Power.h>
+using namespace std;
+
+int main()
+{
+    Power p1;
+    p1.in1();
+    p1.show1();
+    return 0;
+}
