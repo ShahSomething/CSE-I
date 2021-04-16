ae4b1a8cd7977c4c1a212f1ac0c5c0f685fa4baa
Shah Raza <63047096+ShahSomething@users.noreply.github.com>
2021-04-16 19:59:45 +0500

    Create FCFS.c

---
+++ 4th Semester/OS Lab/Lab 8/Task  2/FCFS/FCFS.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+const int pid=0;
+const int arrival=1;
+const int burst=2;
+const int start=3;
+const int end=4;
+const int wait=5;
+const int tat=6;
+const int info=7;
+const int num_p=4;
+int main()
+{
+	int process[num_p][info],i;
+	for(i=0;i<num_p;i++)
+	{
+		process[i][pid]=i;
+		process[i][arrival]=i;
+		printf("Enter the Burst time for Process %d: \n",i);
+		scanf("%d",&process[i][burst]);
+		if(!i)
+			process[i][start]=0;
+		else
+			process[i][start]=process[i-1][end];
+		process[i][end]=process[i][start]+process[i][burst];
+		process[i][wait]=process[i][start]-process[i][arrival];
+		process[i][tat]=process[i][wait]+process[i][burst];
+	}
+	printf("PID	Arrival	Burst	Start	End	Wait	TAT\n");
+	for(i=0;i<num_p;i++)
+	{
+		printf("%d \t %d \t %d \t %d \t %d \t %d \t %d \n",process[i][pid],process[i][arrival],
+		process[i][burst],process[i][start],process[i][end],process[i][wait],process[i][tat]);	
+	}
+}
