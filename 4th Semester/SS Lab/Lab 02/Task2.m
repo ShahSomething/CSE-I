689516382e098a71ee38a067db11061a8d903752
Shah Raza <63047096+ShahSomething@users.noreply.github.com>
2021-04-16 22:45:13 +0500

    Add files via upload

---
+++ 4th Semester/SS Lab/Lab 02/Task2.m
@@ -0,0 +1,6 @@
+clc
+clear all
+close all
+A = rand(1,2500);
+B = rand(1,2500);
+C = A+B
---
+++ 4th Semester/SS Lab/Lab 02/Task7.m
@@ -0,0 +1,10 @@
+clc
+clear all
+close all
+A=[-3 5
+4 8];
+Sum= sum(A)
+Product = prod(A)
+Length= length(A)
+Size= size(A)
+
